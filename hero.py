a23bf1aa15838b48ad16097518916647c18495d0
gwizzle651 <gwizzle651@proton.me>
2025-09-03 13:22:34 -0500

    added total_damage and rounds variables to the main game loop

--- hero.py
+++ hero.py
@@ -1,7 +1,10 @@
+import random
+
+
 class Hero:
     """
     This is our hero blueprint.
-    
+
     O=('-'Q)
 
     Attributes:
@@ -11,18 +14,23 @@ class Hero:
         (Bonus) defence: A hero's ability to reduce incoming damage.
         (Bonus) special_ability: A unique ability the hero can use.
     """
-    
+
     def __init__(self, name):
-        #TODO Set the hero's name.
-        #TODO Set the hero's health. You might give the hero more health than a goblin.
-        #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
-    
+        self.name = name
+        self.health = 250
+        self.strength = random.randint(1, 100)
+        self.healingPower = 50
 
     def strike(self):
-        # TODO Implement the hero's attack logic. It could be stronger or more consistent than a goblin's.
-    
+        return random.randint(1, self.strength)
+
     def receive_damage(self, damage):
-        # TODO Implement take_damage
-        # TODO We should prevent health from going into the NEGATIVE
-    
-    #TODO define is_alive
+        self.health -= damage
+        if self.health < 0:
+            self.health = 0
+
+    def special_ability(self):
+        self.health += self.healingPower
+
+    def is_alive(self):
+        return self.health > 0
